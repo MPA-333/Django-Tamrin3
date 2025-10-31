73e7f6529aa705fbb0834a87c79e837874bcb032
MPA-333 <peymanaghdam@yahoo.com>
2025-11-01 00:32:05 +0330

    Modify settings.py
    dastrasi be directory media dade shod.
    file urls.py asli eslah shod.
    view mored nazar eslah shod.

--- app/introduction/views.py
+++ app/introduction/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.conf import settings
 
 
 def introView(request):
@@ -7,3 +8,10 @@ def introView(request):
 
 def historyView(request):
     return render(request, "introduction/history.html")
+
+
+def departmentView(request):
+    context = {
+        "load_media": settings.MEDIA_URL
+    }
+    return render(request, "introduction/department.html", context)
