81754127bc971d463065e714697ade52c7b84872
MPA-333 <peymanaghdam@yahoo.com>
2025-11-26 01:56:21 +0330

    Create Article View
    view sakhte shod. template khali ast.

--- app/article/views.py
+++ app/article/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+from app.article.models import Article
+
 
 class MainView(TemplateView):
     template_name = "article/main.html"
+
+class ArticleView(ListView):
+    model = Article
+    template_name = ""
