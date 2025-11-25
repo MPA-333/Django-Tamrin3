1d67e22e750cd6551952c52512c3e6ac4804af09
MPA-333 <peymanaghdam@yahoo.com>
2025-11-26 01:58:28 +0330

    Create Article urls
    url marbute tanzim shod.

--- app/article/urls.py
+++ app/article/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.article.views import MainView
+from app.article.views import MainView, ArticleView
 
 urlpatterns = [
     path("main/", MainView.as_view(), name="e_home"),
+    path("main/articles/", ArticleView.as_view(), name="articles"),
 ]
