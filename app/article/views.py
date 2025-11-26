ac597dcb47a3cefc937bfd0657b2aa6fb15abb3f
MPA-333 <peymanaghdam@yahoo.com>
2025-11-27 01:44:08 +0330

    Modify Article_List Page
    safhe list magalat eslah shod. jadval ezafe shod.

--- app/article/views.py
+++ app/article/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.conf import settings
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from app.article.models import Article
+from app.article.models import Article, Author
 
 
 class MainView(TemplateView):
@@ -11,4 +11,10 @@ class ArticleView(ListView):
     model = Article
     context_object_name = "article_list"
     template_name = "article/article_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["load_media"] = settings.MEDIA_URL
+        return context
+    
 
