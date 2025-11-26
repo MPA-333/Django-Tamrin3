2ccd80f119e9649d5527be824d8e96fba063cb55
MPA-333 <peymanaghdam@yahoo.com>
2025-11-27 02:44:23 +0330

    Create Article Detail Page
    safhe joziyat magalat sakhte shod.
    view marbute tanzim shod.
    yek view braye modiriyat download file pdf magale neveshte shod.
    url ha tanzim shod.
    safhe list magalat eslah shod.

--- app/article/urls.py
+++ app/article/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from app.article.views import MainView, ArticleView
+from app.article.views import (
+    MainView,
+    ArticleView,
+    ArticleDetailView,
+    downloadArticleView,
+)
 
 urlpatterns = [
     path("main/", MainView.as_view(), name="e_home"),
     path("main/articles/", ArticleView.as_view(), name="articles"),
+    path("main/article/<int:pk>", ArticleDetailView.as_view(), name="article_detail"),
+    path(
+        "main/article/download_pdf/<int:pk>", downloadArticleView, name="download_pdf"
+    ),
 ]
--- app/article/views.py
+++ app/article/views.py
@@ -1,12 +1,20 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
-from app.article.models import Article, Author
+from django.views.generic.detail import DetailView
+from app.article.models import Article
 
 
+# ====================================================
+# ====================================================
 class MainView(TemplateView):
     template_name = "article/main.html"
 
+
+# ====================================================
+# ====================================================
 class ArticleView(ListView):
     model = Article
     context_object_name = "article_list"
@@ -16,5 +24,35 @@ class ArticleView(ListView):
         context = super().get_context_data(**kwargs)
         context["load_media"] = settings.MEDIA_URL
         return context
-    
 
+
+# ====================================================
+# ====================================================
+class ArticleDetailView(DetailView):
+    model = Article
+    context_object_name = "article_detail"
+    template_name = "article/article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["load_media"] = settings.MEDIA_URL
+        return context
+
+
+# ====================================================
+# ====================================================
+def downloadArticleView(request, pk):
+    article = Article.objects.get(id=pk)
+    fss = FileSystemStorage()
+    fileName = article.article.name
+    if fss.exists(fileName):
+        with fss.open(fileName) as pdf:
+            response = HttpResponse(pdf, content_type="application/pdf")
+            response["Content-Disposition"] = "attachment; filename=Article.pdf"
+            return response
+    else:
+        return HttpResponseNotFound("فایل مورد نظر یافت نشد")
+
+
+# ====================================================
+# ====================================================
