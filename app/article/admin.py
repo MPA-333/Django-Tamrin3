91c18ed533c7b3618ec40b1df48a6c9a632b1fc6
MPA-333 <peymanaghdam@yahoo.com>
2025-11-26 23:45:01 +0330

    Create Article Admin
    braye barname article file admin neveshte shod.

--- app/article/admin.py
+++ app/article/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from app.article.models import Author, Article, ArticleGroup, ArticleGallery
 
-# Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "family",
+        "email",
+        "phone",
+        "slug",
+        "education",
+        "job",
+        "isActive",
+    ]
+    ordering = ["name", "family", "isActive"]
+    prepopulated_fields = {"slug": ("name", "family")}
+
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [
+        "title",
+        "articleGroup",
+        "author",
+        "image",
+        "slug",
+        "text",
+        "word",
+        "description",
+        "article",
+        "registerDate",
+        "publishDate",
+        "updateDate",
+        "seen",
+        "isActive",
+    ]
+    prepopulated_fields = {"slug": ("title", "author")}
+    ordering = ["title", "author", "isActive"]
+
+
+@admin.register(ArticleGroup)
+class ArticleGroupAdmin(admin.ModelAdmin):
+    list_display = [
+        "title"
+    ]
+
+
+@admin.register(ArticleGallery)
+class ArticleGalleryAdmin(admin.ModelAdmin):
+    list_display = [
+        "article",
+        "image",
+    ]
