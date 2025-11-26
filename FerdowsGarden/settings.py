a9a3d789c51d6b18b140b18c02dadf7698aaa47b
MPA-333 <peymanaghdam@yahoo.com>
2025-11-26 23:48:14 +0330

    Modify Settings.py
    barname article be settings.py ezafe shod.

--- FerdowsGarden/settings.py
+++ FerdowsGarden/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "app.main.apps.MainConfig",
     "app.introduction.apps.IntroductionConfig",
+    "app.article.apps.ArticleConfig",
 ]
 
 MIDDLEWARE = [
